5d62e2a8b9480839e9a4a68659284ff60b32b861
Viraj76 <virajg.7680@gmail.com>
2023-12-18 03:16:28 +0530

    designed sign in fragment and some utils

--- app/src/main/java/com/example/userblinkitclone/Utils.kt
+++ app/src/main/java/com/example/userblinkitclone/Utils.kt
@@ -1,4 +1,12 @@
 package com.example.userblinkitclone
 
+import android.content.Context
+import android.widget.Toast
+
 object Utils {
+
+
+     fun showToast(context: Context, message : String){
+        Toast.makeText(context,message , Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/example/userblinkitclone/auth/SignInFragment.kt
+++ app/src/main/java/com/example/userblinkitclone/auth/SignInFragment.kt
@@ -2,26 +2,74 @@ package com.example.userblinkitclone.auth
 
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.userblinkitclone.R
+import com.example.userblinkitclone.Utils
+import com.example.userblinkitclone.databinding.FragmentSingInBinding
 
 
-class SingInFragment : Fragment() {
-
-
+class SignInFragment : Fragment() {
+    private lateinit var binding : FragmentSingInBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        binding = FragmentSingInBinding.inflate(layoutInflater)
         setStatusBarColor()
-        return inflater.inflate(R.layout.fragment_sing_in, container, false)
+
+        getUserNumber()
+
+        onContinueButtonClick()
+        return binding.root
     }
 
+    private fun onContinueButtonClick() {
+        binding.btnContinue.setOnClickListener {
+            val number = binding.etUserNumber.text.toString()
+
+            if(number.isEmpty() || number.length != 10){
+                Utils.showToast(requireContext() , "Please enter valid phone number")
+            }
+            else{
+                val bundle = Bundle()
+                bundle.putString("number" , number)
+                findNavController().navigate(R.id.action_singInFragment_to_OTPFragment , bundle)
+            }
+        }
+    }
+
+    private fun getUserNumber() {
+        binding.etUserNumber.addTextChangedListener ( object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(number: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val  len = number?.length
+
+                if(len == 10){
+                    binding.btnContinue.setBackgroundColor(ContextCompat.getColor(requireContext() , R.color.green))
+                }
+                else{
+                    binding.btnContinue.setBackgroundColor(ContextCompat.getColor(requireContext() , R.color.grayish_blue))
+                }
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+    }
 
 
     private fun setStatusBarColor() {
