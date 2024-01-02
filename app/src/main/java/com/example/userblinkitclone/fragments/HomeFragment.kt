9ed1d2fbc5c6cbbe0432ec3862a91b938e5528e0
Viraj76 <virajg.7680@gmail.com>
2024-01-03 01:25:47 +0530

    showing bestsellers and categories of all products

--- app/src/main/java/com/example/userblinkitclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/userblinkitclone/fragments/HomeFragment.kt
@@ -1,28 +1,80 @@
-package com.example.userblinkitclone
+package com.example.userblinkitclone.fragments
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.userblinkitclone.utils.CartListener
+import com.example.userblinkitclone.utils.Constants
+import com.example.userblinkitclone.R
+import com.example.userblinkitclone.utils.Utils
+import com.example.userblinkitclone.adapters.AdapterBestseller
 import com.example.userblinkitclone.adapters.AdapterCategory
+import com.example.userblinkitclone.adapters.AdapterProduct
+import com.example.userblinkitclone.databinding.BsSeeAllBinding
 import com.example.userblinkitclone.databinding.FragmentHomeBinding
+import com.example.userblinkitclone.databinding.ItemViewProductBinding
+import com.example.userblinkitclone.models.Bestseller
 import com.example.userblinkitclone.models.Category
+import com.example.userblinkitclone.models.Product
+import com.example.userblinkitclone.roomdb.CartProductTable
+import com.example.userblinkitclone.viewmodels.UserViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding  : FragmentHomeBinding
+    private val viewModel : UserViewModel by viewModels()
+    private lateinit var adapterBestseller: AdapterBestseller
+    private lateinit var adapterProduct: AdapterProduct
+    private var cartListener : CartListener? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding  = FragmentHomeBinding.inflate(layoutInflater)
         setStatusBarColor()
         setAllCategories()
+        navigatingToSearchFragment()
+        onProfileClicked()
 
+        fetchBestseller()
         return binding.root
+    }
+
+    private fun fetchBestseller() {
+        binding.shimmerViewContainer.visibility = View.VISIBLE
+        lifecycleScope.launch {
+            viewModel.fetchProductTypes().collect{
+                adapterBestseller = AdapterBestseller(::onSeeAllButtonClicked)
+                binding.rvBestsellers.adapter = adapterBestseller
+                adapterBestseller.differ.submitList(it)
+                binding.shimmerViewContainer.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun onProfileClicked() {
+        binding.ivProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+    }
+
+
+    private fun navigatingToSearchFragment() {
+        binding.searchCv.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+        }
     }
 
     private fun setAllCategories() {
@@ -31,8 +83,123 @@ class HomeFragment : Fragment() {
         for(i in 0 until Constants.allProductsCategoryIcon.size){
             categoryList.add(Category(Constants.allProductsCategory[i] , Constants.allProductsCategoryIcon[i]))
         }
+        binding.rvCategories.layoutManager = GridLayoutManager(requireContext(), 4)
+        binding.rvCategories.adapter = AdapterCategory(categoryList, ::onCategoryIconClicked)
 
-        binding.rvCategories.adapter = AdapterCategory(categoryList)
+    }
+    fun onCategoryIconClicked(category: Category) {
+        val bundle = Bundle()
+        bundle.putString("category" , category.title)
+        findNavController().navigate(R.id.action_homeFragment_to_categoryFragment , bundle)
+    }
+
+    fun onSeeAllButtonClicked(productType : Bestseller){
+        val bsSeeAllBinding = BsSeeAllBinding.inflate(LayoutInflater.from(requireContext()))
+        val bs = BottomSheetDialog(requireContext())
+        bs.setContentView(bsSeeAllBinding.root)
+
+        adapterProduct = AdapterProduct(::onAddButtonClicked , ::onIncrementButtonClicked , ::onDecrementButtonClicked)
+        bsSeeAllBinding.rvProducts.adapter = adapterProduct
+        adapterProduct.differ.submitList(productType.products)
+        bs.show()
+    }
+
+    private fun onAddButtonClicked(product: Product, productBinding: ItemViewProductBinding){
+        productBinding.tvAdd.visibility = View.GONE
+        productBinding.llProductCount.visibility = View.VISIBLE
+
+
+        //step 1.
+        var itemCount = productBinding.tvProductCount.text.toString().toInt()
+        itemCount++
+
+        productBinding.tvProductCount.text = itemCount.toString()
+
+        cartListener?.showCartLayout(1)
+
+
+        //step 2.
+        product.itemCount = itemCount
+        lifecycleScope.launch {
+            cartListener?.savingCartItemCount(1)
+            saveProductInRoomDb(product)
+            viewModel.updateItemCount(product , itemCount)
+        }
+
+    }
+
+    private fun onIncrementButtonClicked(product: Product, productBinding: ItemViewProductBinding){
+
+        var itemCountInc = productBinding.tvProductCount.text.toString().toInt()
+        itemCountInc++
+
+
+        if(product.productStock!! + 1 > itemCountInc){
+            productBinding.tvProductCount.text = itemCountInc.toString()
+
+            cartListener?.showCartLayout(1)
+
+            //step 2.
+            product.itemCount = itemCountInc
+            lifecycleScope.launch {
+                cartListener?.savingCartItemCount(1)
+                saveProductInRoomDb(product)
+                viewModel.updateItemCount(product , itemCountInc)
+            }
+        }
+
+        else{
+            Utils.showToast(requireContext() , "Can't add more item of this")
+        }
+
+
+    }
+    private fun onDecrementButtonClicked(product: Product, productBinding: ItemViewProductBinding){
+
+        var itemCountDec = productBinding.tvProductCount.text.toString().toInt()
+        itemCountDec--
+
+        product.itemCount = itemCountDec
+        lifecycleScope.launch {
+            cartListener?.savingCartItemCount(-1)
+            saveProductInRoomDb(product)
+            viewModel.updateItemCount(product , itemCountDec)
+        }
+
+        if(itemCountDec > 0){
+            productBinding.tvProductCount.text = itemCountDec.toString()
+        }
+        else{
+            lifecycleScope.launch { viewModel.deleteCartProduct(product.productRandomId!!) }
+            Log.d("VV" , product.productRandomId!!)
+            productBinding.tvAdd.visibility = View.VISIBLE
+            productBinding.llProductCount.visibility = View.GONE
+            productBinding.tvProductCount.text = "0"
+        }
+
+        cartListener?.showCartLayout(-1)
+
+        //step 2
+
+    }
+
+    private fun saveProductInRoomDb(product: Product) {
+
+        val cartProduct = CartProductTable(
+            productId = product.productRandomId!!,
+            productTitle = product.productTitle,
+            productQuantity = product.productQuantity.toString() + product.productUnit.toString(),
+            productPrice = "₹" + "${product.productPrice}",
+            productCount = product.itemCount,
+            productStock = product.productStock,
+            productImage = product.productImageUris?.get(0)!!,
+            productCategory = product.productCategory,
+            adminUid  = product.adminUid,
+            productType = product.productType
+
+        )
+
+        lifecycleScope.launch { viewModel.insertCartProduct(cartProduct) }
 
     }
 
@@ -44,5 +211,16 @@ class HomeFragment : Fragment() {
                 decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             }
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is CartListener){
+            cartListener = context
+        }
+        else{
+            throw ClassCastException("Please implement cart listener")
+        }
+
     }
 }
