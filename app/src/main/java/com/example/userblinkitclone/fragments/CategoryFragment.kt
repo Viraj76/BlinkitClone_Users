f3731f29a721104ab18a23823da75e7116e2ede9
Viraj76 <virajg.7680@gmail.com>
2024-01-03 01:26:33 +0530

    showing products of a particular category

--- app/src/main/java/com/example/userblinkitclone/fragments/CategoryFragment.kt
+++ app/src/main/java/com/example/userblinkitclone/fragments/CategoryFragment.kt
@@ -1,59 +1,215 @@
-package com.example.userblinkitclone
+package com.example.userblinkitclone.fragments
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.userblinkitclone.utils.CartListener
+import com.example.userblinkitclone.R
+import com.example.userblinkitclone.utils.Utils
+import com.example.userblinkitclone.adapters.AdapterProduct
+import com.example.userblinkitclone.databinding.FragmentCategoryBinding
+import com.example.userblinkitclone.databinding.ItemViewProductBinding
+import com.example.userblinkitclone.models.Product
+import com.example.userblinkitclone.roomdb.CartProductTable
+import com.example.userblinkitclone.viewmodels.UserViewModel
+import kotlinx.coroutines.launch
+import kotlin.ClassCastException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentCategoryBinding
+    private val viewModel : UserViewModel  by viewModels()
+    private  var category : String ? = null
+    private lateinit var adapterProduct: AdapterProduct
+    private var cartListener : CartListener? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        binding = FragmentCategoryBinding.inflate(layoutInflater)
+        setStatusBarColor()
+        getProductCategory()
+        onNavigationIconClick()
+        setToolBarTitle()
+        onSearchMenuClick()
+        fetchCategoryProduct()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun onNavigationIconClick() {
+        binding.tbSearchFragment.setNavigationOnClickListener {
+            findNavController().navigate(R.id.action_categoryFragment_to_homeFragment)
+        }
     }
+
+    private fun onSearchMenuClick() {
+        binding.tbSearchFragment.setOnMenuItemClickListener {menuItem->
+            when(menuItem.itemId){
+                R.id.searhMenu ->{
+                    findNavController().navigate(R.id.action_categoryFragment_to_searchFragment)
+                    true
+                }
+
+                else -> {false}
+            }
+        }
+    }
+
+    private fun fetchCategoryProduct() {
+        binding.shimmerViewContainer.visibility = View.VISIBLE
+        lifecycleScope.launch {
+            viewModel.getCategoryProduct(category!!).collect{
+                if(it.isEmpty()){
+                    binding.rvProducts.visibility = View.GONE
+                    binding.tvText.visibility = View.VISIBLE
+                }
+                else{
+                    binding.rvProducts.visibility = View.VISIBLE
+                    binding.tvText.visibility = View.GONE
+                }
+                adapterProduct = AdapterProduct(::onAddButtonClicked , ::onIncrementButtonClicked , ::onDecrementButtonClicked)
+                binding.rvProducts.adapter = adapterProduct
+                adapterProduct.differ.submitList(it)
+                binding.shimmerViewContainer.visibility = View.GONE
+            }
+        }
+
+    }
+
+    private fun setToolBarTitle() {
+        binding.tbSearchFragment.title = category
+    }
+
+    private fun getProductCategory() {
+        val bundle = arguments
+        category = bundle?.getString("category")
+    }
+
+    private fun onAddButtonClicked(product: Product, productBinding: ItemViewProductBinding){
+        productBinding.tvAdd.visibility = View.GONE
+        productBinding.llProductCount.visibility = View.VISIBLE
+
+
+        //step 1.
+        var itemCount = productBinding.tvProductCount.text.toString().toInt()
+        itemCount++
+
+        productBinding.tvProductCount.text = itemCount.toString()
+
+        cartListener?.showCartLayout(1)
+
+
+        //step 2.
+        product.itemCount = itemCount
+        lifecycleScope.launch {
+            cartListener?.savingCartItemCount(1)
+            saveProductInRoomDb(product)
+            viewModel.updateItemCount(product , itemCount)
+        }
+
+    }
+
+    private fun onIncrementButtonClicked(product: Product, productBinding: ItemViewProductBinding){
+
+        var itemCountInc = productBinding.tvProductCount.text.toString().toInt()
+        itemCountInc++
+
+
+        if(product.productStock!! + 1 > itemCountInc){
+            productBinding.tvProductCount.text = itemCountInc.toString()
+
+            cartListener?.showCartLayout(1)
+
+            //step 2.
+            product.itemCount = itemCountInc
+            lifecycleScope.launch {
+                cartListener?.savingCartItemCount(1)
+                saveProductInRoomDb(product)
+                viewModel.updateItemCount(product , itemCountInc)
+            }
+        }
+
+        else{
+            Utils.showToast(requireContext() , "Can't add more item of this")
+        }
+
+
+    }
+    private fun onDecrementButtonClicked(product: Product, productBinding: ItemViewProductBinding){
+
+        var itemCountDec = productBinding.tvProductCount.text.toString().toInt()
+        itemCountDec--
+
+        product.itemCount = itemCountDec
+        lifecycleScope.launch {
+            cartListener?.savingCartItemCount(-1)
+            saveProductInRoomDb(product)
+            viewModel.updateItemCount(product , itemCountDec)
+        }
+
+        if(itemCountDec > 0){
+            productBinding.tvProductCount.text = itemCountDec.toString()
+        }
+        else{
+            lifecycleScope.launch { viewModel.deleteCartProduct(product.productRandomId!!) }
+            Log.d("VV" , product.productRandomId!!)
+            productBinding.tvAdd.visibility = View.VISIBLE
+            productBinding.llProductCount.visibility = View.GONE
+            productBinding.tvProductCount.text = "0"
+        }
+
+        cartListener?.showCartLayout(-1)
+
+        //step 2
+
+    }
+
+    private fun saveProductInRoomDb(product: Product) {
+
+        val cartProduct = CartProductTable(
+            productId = product.productRandomId!!,
+            productTitle = product.productTitle,
+            productQuantity = product.productQuantity.toString() + product.productUnit.toString(),
+            productPrice = "₹" + "${product.productPrice}",
+            productCount = product.itemCount,
+            productStock = product.productStock,
+            productImage = product.productImageUris?.get(0)!!,
+            productCategory = product.productCategory,
+            adminUid  = product.adminUid,
+            productType = product.productType
+
+        )
+
+        lifecycleScope.launch { viewModel.insertCartProduct(cartProduct) }
+
+    }
+    private fun setStatusBarColor() {
+        activity?.window?.apply {
+            val statusBarColors = ContextCompat.getColor(requireContext(), R.color.yellow)
+            statusBarColor = statusBarColors
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is CartListener){
+            cartListener = context
+        }
+        else{
+            throw ClassCastException("Please implement cart listener")
+        }
+
+    }
+
 }
